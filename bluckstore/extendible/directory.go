33397b1b8a44ddc63a5ac918c5d6e4516b67a230
Joyen-Conseil Benjamin <bjoyenconseil@octo.com>
2016-05-12 02:02:11 +0200

    directory : Put extendible hashing implem

--- bluckstore/extendible/directory.go
+++ bluckstore/extendible/directory.go
@@ -1,22 +1,64 @@
 package extendible
 
 import (
-	"github.com/BenJoyenConseil/bluckdb/bluckstore/extendible/page"
-	"errors"
+	page "github.com/BenJoyenConseil/bluckdb/bluckstore/extendible/page"
 	"github.com/BenJoyenConseil/bluckdb/util"
+	"os"
 )
 
 type Directory struct {
 	globalDepth uint64
-	pointerPageTable []extendible.Page
+	pointerPageTable []page.Page
+	file *os.File
 }
 
-func (self *Directory) getPage(key util.Hashable) extendible.Page {
-	return self.pointerPageTable[key.Hash() & (( 1 << self.globalDepth) -1)]
+func NewDiskDirectory() *Directory{
+	return nil
+}
+
+func (self *Directory) getPage(key util.Hashable) page.Page {
+	return self.pointerPageTable[self.extendibleHash(key)]
+}
+
+func (self *Directory) extendibleHash(key util.Hashable) int {
+	return key.Hash() & (( 1 << self.globalDepth) -1)
 }
 
 func (self *Directory) Put(key, value string) error {
-	return errors.New("Not implemented")
+	pageWhereToPutTheRecord := self.getPage(util.String(key))
+	fullError := pageWhereToPutTheRecord.Put(key, value)
+
+	if fullError != nil {
+		if self.globalDepth == pageWhereToPutTheRecord.LocalDepth() {
+		 	self.pointerPageTable = append(self.pointerPageTable, self.pointerPageTable...)
+			self.globalDepth += 1
+		}
+
+		newPage1 := page.New()
+		newPage2 := page.New()
+
+		for i, p := range self.pointerPageTable {
+			if p == pageWhereToPutTheRecord {
+				if (i >> pageWhereToPutTheRecord.LocalDepth()) & 1 == 1 {
+					self.pointerPageTable[i] = newPage1
+				} else {
+					self.pointerPageTable[i] = newPage2
+				}
+			}
+		}
+
+		self.Put(key, value)
+		iterator := page.NewRecordIterator(pageWhereToPutTheRecord)
+		for iterator.HasNext() {
+			record := iterator.Next()
+			self.Put(string(record.Key()), string(record.Value()))
+		}
+
+		pageWhereToPutTheRecord.SetLocalDepth(pageWhereToPutTheRecord.LocalDepth() + 1)
+		newPage1.SetLocalDepth(pageWhereToPutTheRecord.LocalDepth())
+	}
+
+	return nil
 }
 
 func (self *Directory) Get(key string) (string, error) {
--- bluckstore/extendible/directory_test.go
+++ bluckstore/extendible/directory_test.go
@@ -10,13 +10,23 @@ import (
 )
 
 
-type stubPage struct{content []byte}
+type stubPage struct{content string; full bool; localDepth uint64}
+
 func (stub *stubPage) Get(key string) (string, error){
 	if key == "123" {return "Hello", nil}
-	return "Stub", errors.New("Stub !!")
+	return "", errors.New("Not implemented !!")
 }
-func (stub *stubPage) Put(key, value string) error {return errors.New("Stub !")}
-func (stub *stubPage) Full(record record.Record) bool{return true}
+func (stub *stubPage) Put(key, value string) error {
+	if key == "full" {
+		return errors.New("the page is full")
+	}
+	return nil
+}
+func (stub *stubPage) Full(record record.Record) bool {return stub.full}
+func (stub *stubPage) LocalDepth() uint64 {return stub.localDepth}
+func (stub *stubPage) SetLocalDepth(num uint64) {stub.localDepth = num}
+func (stub *stubPage) Content() []byte { return []byte(stub.content) }
+
 
 func TestGetPage(t *testing.T) {
 	// Given
@@ -25,18 +35,15 @@ func TestGetPage(t *testing.T) {
 		globalDepth: 1,
 	}
 	key := util.String("123")
-	key2 := util.String("122")
-	var pointerTable *stubPage = &stubPage{}
-	dir.pointerPageTable[1] = pointerTable
+	var pointerPage = &stubPage{}
+	dir.pointerPageTable[1] = pointerPage
 
 	// When
 	result := dir.getPage(key)
-	result2 := dir.getPage(key2)
 
 	// Then
-	var expected *stubPage = pointerTable
+	var expected = pointerPage
 	assert.True(t, expected == result)
-	assert.Nil(t, result2)
 }
 
 func TestGet(t *testing.T) {
@@ -45,8 +52,7 @@ func TestGet(t *testing.T) {
 		pointerPageTable: make([]page.Page, 2),
 		globalDepth: 1,
 	}
-	var pointerTable *stubPage = &stubPage{}
-	dir.pointerPageTable[1] = pointerTable
+	dir.pointerPageTable[1] = &stubPage{}
 
 	// When
 	result, err := dir.Get("123")
@@ -54,4 +60,174 @@ func TestGet(t *testing.T) {
 	// Then
 	assert.Equal(t, "Hello", result)
 	assert.Nil(t, err)
+}
+
+func TestExtendibleHashing (t *testing.T) {
+	// Given
+	dir := &Directory{ globalDepth: 0}
+
+	// When
+	result := dir.extendibleHash(util.String("key"))
+
+	// Then
+	assert.Equal(t, 0, result)
+}
+
+func TestExtendibleHashing_whenGlobalDepthIsEqualTo3_shouldReturn4 (t *testing.T) {
+	// Given
+	dir := &Directory{ globalDepth: 3}
+
+	// When
+	result := dir.extendibleHash(util.String("key"))
+
+	// Then
+	assert.Equal(t, 4, result)
+}
+
+
+func TestExtendibleHashing_whenGlobalDepthIsEqualTo6_shouldReturn44 (t *testing.T) {
+	// Given
+	dir := &Directory{ globalDepth: 6}
+
+	// When
+	result := dir.extendibleHash(util.String("key"))
+
+	// Then
+	assert.Equal(t, 44, result)
+}
+
+/*
+To split a bucket/page j when inserting record with search-key value Kj :
+Update the second half of the bucket/page address table entries originally pointing to j, to point to z
+ */
+func TestPut_WhenPageIsFull_WhenLocalDepthIsLowerthanGlobal_ShouldIncrementLocalDepth(t *testing.T) {
+	// Given
+	dir := &Directory{
+		pointerPageTable: make([]page.Page, 2),
+		globalDepth: 1,
+	}
+	p1 := &stubPage{
+		localDepth: 0,
+		full: true,
+	}
+	dir.pointerPageTable[0] = p1
+	dir.pointerPageTable[1] = p1
+	value := ""
+	key := "full"
+
+	// When
+	dir.Put(key, value)
+
+	// Then
+	assert.Equal(t, 1, int(dir.pointerPageTable[0].LocalDepth()))
+	assert.Equal(t, 1, int(dir.pointerPageTable[1].LocalDepth()))
+}
+
+func TestPut_WhenPageIsFull_WhenLocalDepthIsLowerthanGlobal_ShouldCreateNewPage(t *testing.T) {
+	// Given
+	dir := &Directory{
+		pointerPageTable: make([]page.Page, 2),
+		globalDepth: 1,
+	}
+	p1 := &stubPage{
+		localDepth: 0,
+		full: true,
+	}
+	dir.pointerPageTable[0] = p1
+	dir.pointerPageTable[1] = p1
+	value := ""
+	key := "full"
+
+	// When
+	dir.Put(key, value)
+
+	// Then
+	assert.Equal(t, p1, dir.pointerPageTable[0])
+	assert.NotEqual(t, p1, dir.pointerPageTable[1])
+}
+
+func TestPut_WhenPageIsFull_WhenLocalDepthIsLowerthanGlobal_ShouldRebalanceRecord(t *testing.T) {
+	// Given
+	record1 := string([]byte{0x2, 0x0, 0x0, 0x0, 'r', '1'})
+	record2 := string([]byte{0x2, 0x0, 0x0, 0x0, 'r', '2'})
+	record3 := string([]byte{0x2, 0x0, 0x0, 0x0, 'r', '3'})
+	dir := &Directory{
+		pointerPageTable: make([]page.Page, 2),
+		globalDepth: 1,
+	}
+	p1 := &stubPage{
+		localDepth: 0,
+		full: true,
+		content: record1 + record2 + record3,
+	}
+	dir.pointerPageTable[0] = p1
+	dir.pointerPageTable[1] = p1
+	key := "full"
+	value := "r4"
+
+	// When
+	dir.Put(key, value)
+
+	// Then
+	assert.Equal(t, "\x04\x00\x02\x00fullr4\x02\x00\x00\x00r1\x02\x00", string(dir.pointerPageTable[0].Content()[:18]))
+	assert.Equal(t, "\x02\x00\x00\x00r2", string(dir.pointerPageTable[1].Content()[:6]))
+}
+
+func TestPut_WhenPageIsFull_AndLocalDepthEqualToGlobal_ShouldIncrementGlobalDepth(t *testing.T) {
+	// Given
+	depth := uint64(1)
+	dir := &Directory{
+		pointerPageTable: make([]page.Page, 2),
+		globalDepth: depth,
+	}
+	p1 := &stubPage{
+		localDepth: depth,
+		full: true,
+	}
+
+	p2 := &stubPage{
+		localDepth: depth,
+		full: true,
+	}
+	dir.pointerPageTable[0] = p1
+	dir.pointerPageTable[1] = p2
+	value := ""
+	key := "full"
+
+	// When
+	dir.Put(key, value)
+
+	// Then
+	assert.Equal(t, 2, int(dir.globalDepth))
+}
+
+func TestPut_WhenPageIsFull_AndLocalDepthEqualToGlobal_ShouldMultiplyByTwoThePointerPageTable(t *testing.T) {
+	// Given
+	depth := uint64(1)
+	dir := &Directory{
+		pointerPageTable: make([]page.Page, 2),
+		globalDepth: depth,
+	}
+	p1 := &stubPage{
+		localDepth: depth,
+		full: true,
+	}
+
+	p2 := &stubPage{
+		localDepth: depth,
+		full: true,
+	}
+	dir.pointerPageTable[0] = p1
+	dir.pointerPageTable[1] = p2
+	value := ""
+	key := "full"
+
+	// When
+	dir.Put(key, value)
+
+	// Then
+	assert.Equal(t, 4, len(dir.pointerPageTable))
+	pointerAtIndex1 := dir.pointerPageTable[1]
+	pointerAtIndex3 := dir.pointerPageTable[3]
+	assert.True(t, pointerAtIndex1 == pointerAtIndex3)
 }
