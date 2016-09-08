f876576265068c754cf26e18060dc4c708900c16
Joyen-Conseil Benjamin <bjoyenconseil@octo.com>
2016-09-09 00:21:31 +0200

    moving hashmap reference benchmark to a specific file

---
+++ bluckstore/mmap/hashmap_test.go
@@ -0,0 +1,34 @@
+package memap
+
+import (
+	"testing"
+	"strconv"
+	"os"
+	"math/rand"
+)
+
+func BenchmarkHashmapGet(b *testing.B) {
+	store := make(map[string]string)
+	size := 10000
+	for i := 0; i < size; i++ {
+		store[strconv.Itoa(i)] = "mec, elle est où ma caisse ??"
+	}
+	devNull, _ := os.Open(os.DevNull)
+	defer devNull.Close()
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		devNull.Write([]byte(store[strconv.Itoa(rand.Intn(10000-1))]))
+	}
+}
+
+
+
+func BenchmarkHashmapPut(b *testing.B) {
+	store := make(map[string]string)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		store[strconv.Itoa(n)] = "mec, elle est où ma caisse ??"
+	}
+}
